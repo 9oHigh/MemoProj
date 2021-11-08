5ce1403c9ee544cafd32c35d94767fc7e2ec5b93
9oHigh <wkdska957@naver.com>
2021-11-08 20:17:42 +0900

    Init

---
+++ MemoProj/MemoProj/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MemoProj
+//
+//  Created by 이경후 on 2021/11/08.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
